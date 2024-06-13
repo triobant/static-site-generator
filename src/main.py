1ffc384493683fd70546285b6ab98e9d856c3748
Brian Ott <bott@b1-systems.de>
2024-06-13 16:30:04 +0200

    change: corrected the mistakes in textnode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    txt_obj = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(txt_obj)
+
+
+if __name__ == '__main__':
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,17 @@
 class TextNode:
-    self.text = text
-    self.text_type = text_type
-    self.url = None
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
 
-    def __eg__():
-        return self.text == self.text_type
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return (self.text == other.text and
+                    self.text_type == other.text_type and
+                    self.url == other.url)
+        return False
 
 
-    def __repr__():
-        return f"{TextNode(text, text_type, url)}"
+    def __repr__(self):
+        return f"TextNode({self.text, self.text_type, self.url})"
