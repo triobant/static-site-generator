be9cb77401e3f7cb3318247effb07516acdd76d4
Brian Ott <bott@b1-systems.de>
2024-06-13 12:34:05 +0200

    add: initial textnode file

---
+++ src/textnode.py
@@ -0,0 +1,12 @@
+class TextNode:
+    self.text = text
+    self.text_type = text_type
+    self.url = None
+
+
+    def __eg__():
+        return self.text == self.text_type
+
+
+    def __repr__():
+        return f"{TextNode(text, text_type, url)}"
