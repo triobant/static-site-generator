9cf3f9365552df0a410521363b37811a28abbf2e
Brian Ott <bott@b1-systems.de>
2024-06-16 16:44:14 +0200

    change: default value to None

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,5 +1,5 @@
 class HTMLNode:
-    def __init__(self, tag, value, children, props):
+    def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
         self.children = children
