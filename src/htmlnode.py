bdd8cc04c159bfa3d8b57ae5a726edbb3333a99d
Brian Ott <bott@b1-systems.de>
2024-06-16 16:24:53 +0200

    add: methods for htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,15 @@
+class HTMLNode:
+    def __init__(self):
+        ...
+
+
+    def to_html(self):
+        ...
+
+
+    def props_to_html(self):
+        ...
+
+
+    def __repr__(self):
+        ...
