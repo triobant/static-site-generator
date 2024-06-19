99b01ad796b838b985cc974ba75ef75279641a4f
Brian Ott <bott@b1-systems.de>
2024-06-19 12:41:17 +0200

    change: to_html Leafnode constructs the correct html string

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,6 +24,20 @@ class HTMLNode:
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
-class LeafNode:
-    def __init__(self):
-        ...
+class LeafNode(HTMLNode):
+    def __init__(self, tag = None, value = None, props = None):
+        super().__init__(tag, None, props)
+        self.value = value
+
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have a value.")
+
+
+        if self.tag is None:
+            return self.value
+
+
+        props_str = self.props_to_html()
+        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
