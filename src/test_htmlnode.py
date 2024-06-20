b58cac6d7a533785f41e50315e618d6a71438403
Brian Ott <bott@b1-systems.de>
2024-06-20 12:48:47 +0200

    create: test for LeafNode without children in test_htlmnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
@@ -13,6 +13,11 @@ class TestHTMLNode(unittest.TestCase):
                 node.props_to_html(),
                 ' class="greeting" href="https://boot.dev"',
         )
+
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 
 if __name__ == "__main__":
