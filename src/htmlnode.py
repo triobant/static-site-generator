da94caf00f476706bc41a623379356ed7eaf5031
Brian Ott <bott@b1-systems.de>
2024-06-20 12:37:56 +0200

    update: LeafNode repr

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,19 +25,18 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag = None, value = None, props = None):
-        super().__init__(tag, None, props)
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
         self.value = value
 
 
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode must have a value.")
-
-
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
 
-        props_str = self.props_to_html()
-        return f"<{self.tag}{props_str}>{self.value}</{self.tag}>"
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
