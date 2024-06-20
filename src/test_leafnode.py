d69dc66add428388882b71bdeb8935a06592f9db
Brian Ott <bott@b1-systems.de>
2024-06-20 12:21:27 +0200

    update: testing leafnode with tag and value

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -2,7 +2,8 @@ import unittest
 
 class TestLeafNode(unittest.TestCase):
     def test_leafnode_with_tag_and_value(self):
-        ...
+        node = LeafNode(tag="p", value="This is a paragraph.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph.</p>")
 
 
     def test_leanode_without_tag(self):
