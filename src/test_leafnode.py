76d0d6a3ff0e0d5d702967bdc0c98944da07b943
Brian Ott <bott@b1-systems.de>
2024-06-20 12:17:12 +0200

    add: new test cases for LeafNode

---
+++ src/test_leafnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestLeafNode(unittest.TestCase):
+    def test_leafnode_with_tag_and_value(self):
+        ...
+
+
+    def test_leanode_without_tag(self):
+        ...
+
+
+    def test_leafnode_raises_value_error(self):
+        ...
+
+
+    def test_leafnode_with_props(self):
+        ...
+
+
+if __name__ == "__main__":
+    unittest.main()
