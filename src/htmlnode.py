0a519e86166c71f32e1e30177064e95219e729e1
Brian Ott <bott@b1-systems.de>
2024-06-21 23:32:32 +0200

    update: ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -42,12 +42,16 @@ class LeafNode(HTMLNode):
 
 
 class ParentNode(HTMLNode):
-    def __init__(self):
-        ...
+    def __init__(self, tag, children, props):
+        super().__init__(tag, None, children, props)
 
 
     def to_html(self):
-        ...
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        return f"ParentNode({self.tag})"
 
 
     def __repr__(self):
