942403a6df198cf83deedf2f610d01bca492563f
Brian Ott <bott@b1-systems.de>
2024-06-23 11:12:48 +0200

    update: __repr__ of ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -56,4 +56,4 @@ class ParentNode(HTMLNode):
 
 
     def __repr__(self):
-        return f"ParentNode({self.tag})"
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
