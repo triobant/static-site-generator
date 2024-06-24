2d3f128c3632a19620dacb5557c86163c0a5b950
Brian Ott <bott@b1-systems.de>
2024-06-24 07:59:17 +0200

    add: test to check headings

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -73,5 +73,21 @@ class TestHTMLNode(unittest.TestCase):
         )
 
 
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
