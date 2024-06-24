564be794f326bf71562363f31ae0243b8b11d08d
Brian Ott <bott@b1-systems.de>
2024-06-24 07:57:32 +0200

    add: test if ParentNode can have many children

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -57,5 +57,21 @@ class TestHTMLNode(unittest.TestCase):
         )
 
 
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
