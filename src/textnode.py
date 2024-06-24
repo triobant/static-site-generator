c1d53150a3ce3c64cd9dd2bc15e2c59c559eff66
Brian Ott <bott@b1-systems.de>
2024-06-24 12:24:09 +0200

    update: text_node_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -25,17 +25,21 @@ class TextNode:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
 
-    def text_node_to_html_node(text_node):
-        if text_node.text_type == text_type_text:
-            return LeafNode(None, text_node.text)
-        if text_node.text_type == text_type_bold:
-            return LeafNode("b", text_node.text)
-        if text_node.text_type == text_type_italic:
-            return LeafNode("i", text_node.text)
-        if text_node.text_type == text_type_code:
-            return LeafNode("code", text_node.text)
-        if text_node.text_type == text_type_link:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        if text_node.text_type == text_type_image:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-        raise ValueError(f"Invalid text type: {text_node.text_type}")
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+    def split_node_delimiter(old_nodes, delimiter, text_type):
+        ...
