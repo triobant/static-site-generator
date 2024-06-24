1c71c365e167bfeaa5658fcf5ff17adfb4cdbee0
Brian Ott <bott@b1-systems.de>
2024-06-24 08:00:38 +0200

    update: ParentNode constructor

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -42,7 +42,7 @@ class LeafNode(HTMLNode):
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag, children, props):
+    def __init__(self, tag, children, props = None):
         super().__init__(tag, None, children, props)
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
