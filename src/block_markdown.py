ddb7745ba9f21008cc6e5995f9cec5930a89b6b0
Brian Ott <bott@b1-systems.de>
2024-06-26 16:30:45 +0200

    delete: block_markdown.py as it was a duplicate

--- src/block_markdown.py
+++
@@ -1,9 +0,0 @@
-def markdown_to_blocks(markdown):
-    blocks= markdown.split("\n\n")
-    filtered_blocks = []
-    for block in blocks:
-        if block == "":
-            continue
-        block = block.strip()
-        filtered_blocks.append(block)
-    return filtered_blocks
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,18 +1,9 @@
-markdown = """This is **bolded** paragraph
-
-This is another paragraph with *italic* text and `code` here
-This is the same paragraph on a new line
-
-* This is a list
-* with items
-"""
-
 def markdown_to_blocks(markdown):
-    blocks = []
-    splitted = markdown.split("\n\n")
-    for block in splitted:
-        if block != "":
-            blocks.append(block.strip())
-    return blocks
-
-print(markdown_to_blocks(markdown))
+    blocks= markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
