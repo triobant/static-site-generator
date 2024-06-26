29c56a941e0238a773a2c8d98a1576c1828dc9e2
Brian Ott <bott@b1-systems.de>
2024-06-26 11:40:45 +0200

    add: test for text_to_textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -114,6 +114,3 @@ def extract_markdown_links(text):
     pattern = r"\[(.*?)\]\((.*?)\)"
     matches = re.findall(pattern, text)
     return matches
-
-text = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-print(text_to_textnodes(text))
