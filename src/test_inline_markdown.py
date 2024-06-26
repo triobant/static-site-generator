082cf01ca457927c09c3d56c0faaafff41f99527
Brian Ott <bott@b1-systems.de>
2024-06-26 08:17:00 +0200

    update: test_inline_markdown and inline_markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,6 @@
 import unittest
 from inline_markdown import (
-    split_node_delimiter,
-    split_node_delimiter_multiple,
+    split_nodes_delimiter,
 )
 
 from textnode import (
@@ -14,29 +13,12 @@ from textnode import (
 
 
 class TestInlineMarkdown(unittest.TestCase):
-    def test_split_node_delimiter_single(self):
-        node = TextNode("The seventh `import getpass` is a good idea", text_type_text)
-        new_nodes = split_node_delimiter([node], "`", text_type_code)
+    def test_delim_bold(self):
+        node = TextNode("The first **bolded** word node", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
         self.assertListEqual(
             [
-                TextNode("The seventh ", text_type_text),
-                TextNode("import getpass", text_type_code),
-                TextNode(" is a good idea", text_type_text),
-            ],
-            new_nodes
-        )
-
-
-    def test_split_nodes_delimiter_multi(self):
-        node = TextNode("The eigth `import functools` is a good idea", text_type_text)
-        node2 = TextNode("The ninth **bolded** word node", text_type_text)
-        new_nodes = split_node_delimiter_multiple([node, node2], ["`", "**"], [text_type_code, text_type_bold])
-        self.assertListEqual(
-            [
-                TextNode("The eigth", text_type_text),
-                TextNode("import functools", text_type_code),
-                TextNode(" is a good idea", text_type_text),
-                TextNode("The ninth ", text_type_text),
+                TextNode("The first ", text_type_text),
                 TextNode("bolded", text_type_bold),
                 TextNode(" word node", text_type_text)
             ],
@@ -44,4 +26,79 @@ class TestInlineMarkdown(unittest.TestCase):
         )
 
 
-#        node = TextNode("The tenth *italic and **bolded** word* node", text_type_bold)
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "The double **bolded** word and **second** node", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("The double ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word and ", text_type_text),
+                TextNode("second", text_type_bold),
+                TextNode(" node", text_type_text)
+            ],
+            new_nodes
+        )
+
+
+    def test_delim_bold_multiword(self):
+        node = TextNode(
+            "The multiword **bolded word** and **third** node", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("The multiword ", text_type_text),
+                TextNode("bolded word", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("third", text_type_bold),
+                TextNode(" node", text_type_text)
+            ],
+            new_nodes
+        )
+
+
+    def test_delim_italic(self):
+        node = TextNode("The third *italic* word and node", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("The third ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and node", text_type_text),
+            ],
+            new_nodes
+        )
+
+
+    def test_delim_bold_and_italic(self):
+        node = TextNode("This is the fourth **bold** and *italic*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("This is the fourth ", text_type_text),
+                TextNode("bold", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("italic", text_type_italic)
+            ],
+            new_nodes
+        )
+
+
+    def test_delim_code(self):
+        node = TextNode("The fifth `import getpass` is a good idea", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        self.assertListEqual(
+            [
+                TextNode("The fifth ", text_type_text),
+                TextNode("import getpass", text_type_code),
+                TextNode(" is a good idea", text_type_text),
+            ],
+            new_nodes
+        )
+
+if __name__ == "__main__":
+    unittest.main()
