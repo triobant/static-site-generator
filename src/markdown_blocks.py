e25e0646c8ac2b0aa3e559d2d60248621aaeed7d
Brian Ott <bott@b1-systems.de>
2024-06-27 08:33:49 +0200

    add: block types

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ordered_list = "ordered_list"
+block_type_unordered_list = "unordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks= markdown.split("\n\n")
     filtered_blocks = []
@@ -7,3 +15,7 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         filtered_blocks.append(block)
     return filtered_blocks
+
+
+def block_to_block_type(block):
+    ...
