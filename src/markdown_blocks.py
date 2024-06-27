2bd715d02a774e0ffb7097d54e6cb5fc9c416830
Brian Ott <bott@b1-systems.de>
2024-06-27 10:06:45 +0200

    add: function to check which type the block has

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -18,4 +18,44 @@ def markdown_to_blocks(markdown):
 
 
 def block_to_block_type(block):
-    ...
+    lines = block.split("\n")
+
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return block_type_code
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list
+    return block_type_paragraph
