31748a27e0d37a5cb825220a4af2b1aeecabaa6e
Brian Ott <bott@b1-systems.de>
2024-07-01 10:13:51 +0200

    add: test to check if ulist and olist conversion works correctly

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -103,5 +103,24 @@ This is another paragraph with *italic* text and `code` here
             )
 
 
+        def test_lists(self):
+            md = """
+- This is a list
+- with items
+- and *more* items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+            node = markdown_to_html_node(md)
+            html = node.to_html()
+            self.assertEqual(
+                html,
+                "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+            )
+
+
 if __name__ == "__main__":
     unittest.main()
