c11605714a27b813c38130f44a66be824db560a7
Brian Ott <bott@b1-systems.de>
2024-07-01 10:07:31 +0200

    add: test to check if multi paragraphs work

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -86,5 +86,22 @@ tag here
             )
 
 
+        def test_paragraphs(self):
+            md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with *italic* text and `code` here
+
+"""
+            node = markdown_to_html_node(md)
+            html = node.to_html()
+            self.assertEqual(
+                html,
+                "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+            )
+
+
 if __name__ == "__main__":
     unittest.main()
